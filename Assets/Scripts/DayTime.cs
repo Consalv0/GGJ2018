bab3d4d2403e22d155fd9ac9f4a3730aafbadea8
Consalv0 <gonzaloceciliano@gmail.com>
2018-03-04 13:38:45 -0600

    Added Conept Art

--- Assets/Scripts/DayTime.cs
+++ Assets/Scripts/DayTime.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class DayTime : MonoBehaviour {
 	public Gradient skyColor;
 	public Gradient lightColor;
+	public Light sun, moon;
 	public float yEuler;
-	public float dayDuration;
+	public float daySpeed;
 
 	[Range(0, 24)]
 	public float dayTime;
@@ -18,17 +19,29 @@ public class DayTime : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		dayTime += Time.unscaledDeltaTime / dayDuration;
+		dayTime += Time.unscaledDeltaTime / daySpeed;
 		if (dayTime >= 24) dayTime = 0;
-		transform.localRotation = Quaternion.Euler(
-			dayTime / 24 * 360 - 0, yEuler, 0);
+		sun.transform.localRotation = Quaternion.Euler(
+			dayTime / 24 * 360, yEuler, 0);
+		moon.transform.localRotation = Quaternion.Euler(
+			dayTime / 24 * 360 - 200, yEuler + 30, 0);
 
-		if (GetComponent<Light>()) {
+		if (sun) {
 			RenderSettings.ambientLight = skyColor.Evaluate(dayTime / 24);
 			RenderSettings.fogColor = skyColor.Evaluate(dayTime / 24);
+			Camera.main.backgroundColor = skyColor.Evaluate(dayTime / 24);
 			RenderSettings.ambientSkyColor = lightColor.Evaluate(dayTime / 24);
-			Camera.main.backgroundColor = lightColor.Evaluate(dayTime / 24);
-			GetComponent<Light>().color = lightColor.Evaluate(dayTime / 24);
+			sun.color = lightColor.Evaluate(dayTime / 24);
+			sun.intensity = Mathf.Pow(lightColor.Evaluate(dayTime / 24).grayscale, 2);
+		}
+
+		if (moon) {
+			RenderSettings.ambientLight = skyColor.Evaluate(dayTime / 24);
+			RenderSettings.fogColor = skyColor.Evaluate(dayTime / 24);
+			Camera.main.backgroundColor = skyColor.Evaluate(dayTime / 24);
+			RenderSettings.ambientSkyColor = lightColor.Evaluate(dayTime / 24);
+			moon.color = lightColor.Evaluate(dayTime / 24);
+			moon.intensity = 1 - Mathf.Pow(lightColor.Evaluate(dayTime / 24).grayscale, 2);
 		}
 	}
 }
